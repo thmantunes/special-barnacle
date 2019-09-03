a720fe6e4f58d8778f65ba89341c4c815b7ee6ca
Marco Mangan <marco.mangan@pucrs.br>
2019-09-02 22:38:51 -0300

    #1

---
+++ src/test/java/pucrs/qp/triangle/AppTest.java
@@ -0,0 +1,49 @@
+package pucrs.qp.triangle;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testEquilatero()
+    {
+        int actual = App.identificaTriangulo(5, 5, 5);
+        int expected = tipos.EQUILATERO.value();
+        assertEquals(expected, actual);
+    }
+}
+
+
+
+
+
+
+
+
+
